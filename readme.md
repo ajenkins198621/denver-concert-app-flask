9378a8070021338be0bea25f706d519eb7ba3695
Austin Jenkins <austinclarkjenkins@gmail.com>
2024-01-11 06:34:37 -0700

    add unit test for web app

--- readme.md
+++ readme.md
@@ -4,6 +4,10 @@ For Applications of Software Architecture for Big Data class
 ### How to run
 `flask run`
 
+### Testing
+#### To run the unit tests, run:
+`python -m unittest discover -s tests/unit -p '*_test.py'`
+
 ### Useful Links
 [Python Virtualenv Mac](https://gist.github.com/pandafulmanda/730a9355e088a9970b18275cb9eadef3)
 [Flask](https://flask.palletsprojects.com/en/2.3.x/)
---
+++ tests/unit/html_helper_test.py
@@ -0,0 +1,39 @@
+"""
+Unit tests for the htmlhelper.py web application
+"""
+import unittest
+from htmlhelper import HtmlHelper
+
+
+class TestHtmlHelper(unittest.TestCase):
+    """
+    Test the HTML Helper functions
+    """
+
+    def setUp(self):
+        """
+        Instantiate the HtmlHelper class
+        """
+        self.htmlHelper = HtmlHelper()
+
+    def test_gets_header(self):
+        """
+        Tests the get_header() function
+        """
+
+        title = "This is a unit test!"
+        header_content = self.htmlHelper.get_header(title=title)
+        self.assertIsInstance(header_content, str)
+        self.assertIn(title, header_content)
+
+    def test_gets_footer(self):
+        """
+        Tests the get_footer() function
+        """
+
+        footer_content = self.htmlHelper.get_footer()
+        self.assertIsInstance(footer_content, str)
+
+
+if __name__ == '__main__':
+    unittest.main()
